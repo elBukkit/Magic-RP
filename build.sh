eed55d094d815b3495fde271b559e14eb825ba6d
Nathan Wolf <nathan@elmakers.com>
2021-08-02 09:22:44 -0700

    Merge HTTYD RP into "all", update slimefun RP Also probably fixed war RP sounds and maybe other random stuff that was broken but unnoticable in the slew of zip messages (that are now silenced)

--- build.sh
+++ build.sh
@@ -4,24 +4,22 @@ rm -Rf target
 mkdir target
 cd target
 
-# New resource packs
-
 echo "** BUILDING DEFAULT **"
 
 mkdir default
 cd default
 cp -R ../../src/default/* .
 find . -name ".DS_Store" -type f -delete
-zip -r -X ../Magic-RP-1.17.zip *
+zip -q -X -r ../Magic-RP-1.17.zip *
 cd ..
 
-echo "** BUILDING DEFAULT-Skulls **"
+echo "** BUILDING SKULLS **"
 
 mkdir default-skulls
 cd default-skulls
 mkdir assets
 cd assets
-cp -R ../../src/../skulls/assets/* .
+cp -R ../../../src/skulls/assets/* .
 cd ..
 cp -R ../../src/default/* .
 rm -R assets/minecraft/textures/item/spells
@@ -32,17 +30,17 @@ rm -R assets/minecraft/models/item/brushes
 rm assets/minecraft/models/item/diamond_axe.json
 rm assets/minecraft/models/item/diamond_hoe.json
 find . -name ".DS_Store" -type f -delete
-zip -r -X ../Magic-skulls-RP-1.17.zip *
+zip -q -X -r ../Magic-skulls-RP-1.17.zip *
 cd ..
 
-echo "** BUILDING SKULLS **"
+echo "** BUILDING FLAT SKULLS **"
 
 mkdir skulls
 cd skulls
 mkdir assets
 cp -R ../../src/skulls/* .
 find . -name ".DS_Store" -type f -delete
-zip -r -X ../flat-skulls.zip *
+zip -q -X -r ../flat-skulls.zip *
 cd ..
 
 echo "** BUILDING PAINTERLY **"
@@ -52,7 +50,7 @@ cd painterly
 cp -R ../../src/default/* .
 cp -R ../../src/painterly/* .
 find . -name ".DS_Store" -type f -delete
-zip -r -X ../Magic-painterly-RP-1.17.zip *
+zip -q -X -r ../Magic-painterly-RP-1.17.zip *
 cd ..
 
 echo "** BUILDING LOW-RES **"
@@ -62,7 +60,7 @@ cd lowres
 cp -R ../../src/default/* .
 cp -R ../../src/lowres/* .
 find . -name ".DS_Store" -type f -delete
-zip -r -X ../Magic-lowres-RP-1.17.zip *
+zip -q -X -r ../Magic-lowres-RP-1.17.zip *
 cd ..
 
 echo "** BUILDING POTTER **"
@@ -72,7 +70,7 @@ cd potter
 cp -R ../../src/default/* .
 cp -R ../../src/potter/* .
 find . -name ".DS_Store" -type f -delete
-zip -r -X ../Magic-potter-RP-1.17.zip *
+zip -q -X -r ../Magic-potter-RP-1.17.zip *
 cd ..
 
 echo "** BUILDING WAR **"
@@ -81,12 +79,40 @@ mkdir war
 cd war
 cp -R ../../src/war/* .
 
-sed -e '$ d' ../../war/assets/minecraft/sounds.json > assets/minecraft/sounds.json
+sed -e '$ d' ../../src/war/assets/minecraft/sounds.json > assets/minecraft/sounds.json
 echo , >> assets/minecraft/sounds.json
-tail -n +2 ../../war/assets/minecraft/sound-overrides.json >> assets/minecraft/sounds.json
+tail -n +2 ../../src/war/assets/minecraft/sound-overrides.json >> assets/minecraft/sounds.json
 rm assets/minecraft/sound-overrides.json
 find . -name ".DS_Store" -type f -delete
-zip -r -X ../Magic-war-RP-1.17.zip *
+zip -q -X -r ../Magic-war-RP-1.17.zip *
+cd ..
+
+echo "** BUILDING ROBES **"
+
+mkdir robes
+cd robes
+cp -R ../../src/default/* .
+cp -R ../../src/chainmail/assets/minecraft/textures/* assets/minecraft/textures/
+find . -name ".DS_Store" -type f -delete
+zip -q -X -r ../Magic-robes-RP-1.17.zip *
+cd ..
+
+echo "** BUILDING HTTYD **"
+
+mkdir httyd
+cd httyd
+cp -R ../../src/httyd/* .
+find . -name ".DS_Store" -type f -delete
+zip -q -X -r ../Magic-RP-httyd-1.17.zip *
+cd ..
+
+echo "** BUILDING BRAWL **"
+
+mkdir brawl
+cd brawl
+cp -R ../../src/brawl/* .
+find . -name ".DS_Store" -type f -delete
+zip -q -X -r ../Magic-RP-brawl-1.17.zip *
 cd ..
 
 echo "** BUILDING ALL **"
@@ -99,68 +125,43 @@ cp -R ../../src/war/assets/minecraft/sounds/* assets/minecraft/sounds/
 cp -R ../../src/war/assets/minecraft/models/item/* assets/minecraft/models/item/
 cp -R ../../src/war/assets/minecraft/textures/misc assets/minecraft/textures/
 cp -R ../../src/war/assets/minecraft/textures/item/custom/* assets/minecraft/textures/item/custom/
-sed -e '$ d' ../../default/assets/minecraft/sounds.json > assets/minecraft/sounds.json
+sed -e '$ d' ../../src/default/assets/minecraft/sounds.json > assets/minecraft/sounds.json
 echo , >> assets/minecraft/sounds.json
-tail -n +2 ../../war/assets/minecraft/sounds.json >> assets/minecraft/sounds.json
+tail -n +2 ../../src/war/assets/minecraft/sounds.json >> assets/minecraft/sounds.json
+../../merge_folder.php ../../src/httyd .
 find . -name ".DS_Store" -type f -delete
-zip -r -X ../Magic-all-RP-1.17.zip *
-cd ..
-
-echo "** BUILDING MODEL ENGINE **"
-
-mkdir modelengine
-cd modelengine
-cp -R ../../src/default/* .
-cp -R ../../src/chainmail/assets/minecraft/textures/* assets/minecraft/textures/
-cp -R ../../src/war/assets/minecraft/sounds/* assets/minecraft/sounds/
-cp -R ../../src/war/assets/minecraft/models/item/* assets/minecraft/models/item/
-cp -R ../../src/war/assets/minecraft/textures/misc assets/minecraft/textures/
-cp -R ../../src/war/assets/minecraft/textures/item/custom/* assets/minecraft/textures/item/custom/
-
-cp -R ../../src/modelengine/assets/minecraft/models/item/* assets/minecraft/models/item/
-mkdir assets/modelengine
-cp -R ../../src/modelengine/assets/modelengine/* assets/modelengine/
-
-sed -e '$ d' ../../default/assets/minecraft/sounds.json > assets/minecraft/sounds.json
-echo , >> assets/minecraft/sounds.json
-tail -n +2 ../../war/assets/minecraft/sounds.json >> assets/minecraft/sounds.json
-find . -name ".DS_Store" -type f -delete
-zip -r -X ../Magic-modelengine-RP-1.17.zip *
+zip -q -X -r ../Magic-all-RP-1.17.zip *
 cd ..
 
 echo "** BUILDING HIRES **"
 
 mkdir hires
 cd hires
-cp -R ../../src/default/* .
-cp -R ../../src/chainmail/assets/minecraft/textures/* assets/minecraft/textures/
-cp -R ../../src/war/assets/minecraft/sounds/* assets/minecraft/sounds/
-cp -R ../../src/war/assets/minecraft/models/item/* assets/minecraft/models/item/
-cp -R ../../src/war/assets/minecraft/textures/misc assets/minecraft/textures/
-cp -R ../../src/war/assets/minecraft/textures/item/custom/* assets/minecraft/textures/item/custom/
+cp -R ../all/* .
 cp -R ../../src/hires/assets/minecraft/models/item/* assets/minecraft/models/item/
 cp -R ../../src/hires/assets/minecraft/textures/item/* assets/minecraft/textures/item/
-sed -e '$ d' ../../default/assets/minecraft/sounds.json > assets/minecraft/sounds.json
-echo , >> assets/minecraft/sounds.json
-tail -n +2 ../../war/assets/minecraft/sounds.json >> assets/minecraft/sounds.json
 find . -name ".DS_Store" -type f -delete
-zip -r -X ../Magic-hires-RP-1.17.zip *
+zip -q -X -r ../Magic-hires-RP-1.17.zip *
 cd ..
 
-echo "** BUILDING HTTYD **"
+echo "** BUILDING MODEL ENGINE **"
 
-mkdir httyd
-cd httyd
-cp -R ../../src/httyd/* .
+mkdir modelengine
+cd modelengine
+cp -R ../all/* .
+cp -R ../../src/modelengine/assets/minecraft/models/item/* assets/minecraft/models/item/
+mkdir assets/modelengine
+cp -R ../../src/modelengine/assets/modelengine/* assets/modelengine/
 find . -name ".DS_Store" -type f -delete
-zip -r -X ../Magic-RP-httyd-1.17.zip *
+zip -q -X -r ../Magic-modelengine-RP-1.17.zip *
 cd ..
 
-echo "** BUILDING BRAWL **"
+echo "** BUILDING SLIMEFUN **"
 
-mkdir brawl
-cd brawl
-cp -R ../../src/brawl/* .
+mkdir slimefun
+cd slimefun
+cp -R ../all/* .
+../../merge_folder.php ../../src/slimefun .
 find . -name ".DS_Store" -type f -delete
-zip -r -X ../Magic-RP-brawl-1.17.zip *
+zip -q -X -r ../Magic-slimefun-RP-1.17.zip *
 cd ..
--- merge_folder.php
+++ merge_folder.php
@@ -1,1 +1,214 @@
+#!/usr/bin/php
 <?php
+
+if (count($argv) < 3) {
+    die("Usage: merge_folder.php <from> <to> [skip]\n");
+}
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$fromFolder = $argv[1];
+if (!file_exists($fromFolder)) {
+    die("Folder not found: $fromFolder\n");
+}
+
+$toFolder = $argv[2];
+if (!file_exists($toFolder)) {
+    mkdir($toFolder, 0777, true);
+}
+
+$skip = array();
+if (count($argv) > 3) {
+    $skip = explode(';', $argv[3]);
+    $skip = array_fill_keys($skip, true);
+}
+
+function startsWith($haystack, $needle){
+    return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== FALSE;
+}
+
+function endsWith($haystack, $needle) {
+    $length = strlen($needle);
+    return $length === 0 || (substr($haystack, -$length) === $needle);
+}
+
+function overrideSorter($a, $b) {
+    if (isset($a['predicate'])) {
+        if (!isset($b['predicate'])) return -1;
+        $ap = $a['predicate'];
+        $bp = $b['predicate'];
+
+        // CMD
+        if (isset($ap['custom_model_data'])) {
+            if (!isset($bp['custom_model_data'])) return -1;
+            if ($ap['custom_model_data'] < $bp['custom_model_data']) return -1;
+            if ($ap['custom_model_data'] > $bp['custom_model_data']) return 1;
+        }
+        if (isset($bp['custom_model_data'])) return 1;
+
+        // damaged
+        if (isset($ap['damaged'])) {
+            if (!isset($bp['damaged'])) return -1;
+            if ($ap['damaged'] < $bp['damaged']) return -1;
+            if ($ap['damaged'] > $bp['damaged']) return 1;
+        }
+        if (isset($bp['damaged'])) return 1;
+
+        // damage
+        if (isset($ap['damage'])) {
+            if (!isset($bp['damage'])) return -1;
+            if ($ap['damage'] < $bp['damage']) return -1;
+            if ($ap['damage'] > $bp['damage']) return 1;
+        }
+        if (isset($bp['damage'])) return 1;
+
+        // Bow pulling
+        if (isset($ap['pulling'])) {
+            if (!isset($bp['pulling'])) return -1;
+            if ($ap['pulling'] < $bp['pulling']) return -1;
+            if ($ap['pulling'] > $bp['pulling']) return 1;
+        }
+        if (isset($bp['pulling'])) return 1;
+
+        // Bow pull
+        if (isset($ap['pull'])) {
+            if (!isset($bp['pull'])) return -1;
+            if ($ap['pull'] < $bp['pull']) return -1;
+            if ($ap['pull'] > $bp['pull']) return 1;
+        }
+        if (isset($bp['pull'])) return 1;
+
+        // Blocking
+        if (isset($ap['blocking'])) {
+            if (!isset($bp['blocking'])) return -1;
+            if ($ap['blocking'] < $bp['blocking']) return -1;
+            if ($ap['blocking'] > $bp['blocking']) return 1;
+        }
+        if (isset($bp['blocking'])) return 1;
+    }
+    if (isset($b['predicate'])) return 1;
+    return 0;
+}
+
+function copyFile($fromFile, $toFile) {
+    $path = pathinfo($toFile);
+    if (!file_exists($path['dirname'])) {
+        mkdir($path['dirname'], 0777, true);
+    }
+    copy($fromFile, $toFile);
+}
+
+function mergeFolder($fromFolder, $toFolder) {
+    global $skip;
+    if (isset($skip[$fromFolder])) {
+        echo "Skipping $fromFolder\n";
+        return;
+    }
+    foreach (new DirectoryIterator($fromFolder) as $fileInfo) {
+        if ($fileInfo->isDot()) continue;
+        $fileName = $fileInfo->getFilename();
+        if (startsWith($fileName, '.')) continue;
+        $fromFile = $fromFolder . DIRECTORY_SEPARATOR . $fileName;
+        $toFile = $toFolder . DIRECTORY_SEPARATOR . $fileName;
+        if ($fileInfo->isDir()) {
+            mergeFolder($fromFile, $toFile);
+            continue;
+        }
+
+        if (!file_exists($toFile)) {
+            copyFile($fromFile, $toFile);
+            continue;
+        }
+
+        if ($fileInfo->getExtension() != 'json') {
+            if ($fileName !== 'pack.mcmeta' && $fileName !== 'pack.png') {
+                echo " Non-json file exists in both files: $fileName, ignoring second file.\n";
+            }
+            continue;
+        }
+
+        $fromJSON = json_decode(file_get_contents($fromFile), true);
+        $toJSON = json_decode(file_get_contents($toFile), true);
+
+        if (!$toJSON) {
+            copyFile($fromFile, $toFile);
+            continue;
+        }
+
+        if (!$fromJSON) {
+            continue;
+        }
+
+        if (endsWith($fromFile, 'assets/minecraft/sounds.json')) {
+            foreach ($fromJSON as $key => $sound) {
+                if (!isset($toJSON[$key])) {
+                    $toJSON[$key] = $sound;
+                }
+            }
+            file_put_contents($toFile, json_encode($toJSON, JSON_PRETTY_PRINT));
+            continue;
+        }
+
+        if (!isset($fromJSON['overrides'])) {
+            continue;
+        }
+
+        $fromOverrides = $fromJSON['overrides'];
+        if (!isset($toJSON['overrides'])) {
+            $toJSON['overrides'] = $fromOverrides;
+            file_put_contents($toFile, json_encode($toJSON, JSON_PRETTY_PRINT));
+        }
+
+        $toOverrides = $toJSON['overrides'];
+        $hasCustomModelData = array();
+        $hasDamage = array();
+        for ($i = 0; $i < count($toOverrides); $i++) {
+            $override = $toOverrides[$i];
+            if (isset($override['predicate'])) {
+                $predicate = $override['predicate'];
+                if (isset($predicate['custom_model_data'])) {
+                    $hasCustomModelData[$predicate['custom_model_data']] = true;
+                } else if (isset($predicate['damage'])) {
+                    $hasDamage[$predicate['damage']] = true;
+                }
+            }
+        }
+
+        for ($i = 0; $i < count($fromOverrides); $i++) {
+            $override = $fromOverrides[$i];
+            if (isset($override['predicate'])) {
+                $predicate = $override['predicate'];
+                if (isset($predicate['custom_model_data'])) {
+                    if (isset($hasCustomModelData[$predicate['custom_model_data']])) {
+                        echo("Skipping duplicate override from: " . $fromFile . "{CustomModelData:" . $predicate['custom_model_data'] . "}\n");
+                    } else {
+                        array_push($toOverrides, $override);
+                    }
+                } else if (isset($predicate['damage'])) {
+                    if (isset($hasDamage[$predicate['custom_model_data']])) {
+                        echo("Skipping duplicate override from: " . $fromFile . ":" . $predicate['damage'] . "\n");
+                    } else {
+                        array_push($toOverrides, $override);
+                    }
+                } else {
+                    if (!isset($predicate['pulling']) && !isset($predicate['pull']) && !isset($predicate['blocking'])) {
+                        echo("Adding unknown override predicate type from: " . $fromFile . ": " . json_encode($predicate) . "}\n");
+                    }
+                    array_push($toOverrides, $override);
+                }
+            } else {
+                echo("Adding unknown override type from: " . $fromFile . ": " . json_encode($override) . "}\n");
+                array_push($toOverrides, $override);
+            }
+        }
+
+        usort($toOverrides, 'overrideSorter');
+
+        $toJSON['overrides'] = $toOverrides;
+        file_put_contents($toFile, json_encode($toJSON, JSON_PRETTY_PRINT));
+    }
+}
+
+echo "  Merging $fromFolder into $toFolder\n";
+mergeFolder($fromFolder, $toFolder);
