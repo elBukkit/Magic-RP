fe1f8980a7c6febaa3e7a85ace9d84d7efc6e088
Nathan Wolf <nathan@elmakers.com>
2021-08-14 18:26:16 -0700

    Re-work icon generation to take into account models .. it got complicated

--- scripts/generate_icons.php
+++ scripts/generate_icons.php
@@ -4,20 +4,22 @@ if (PHP_SAPI !== 'cli') {
     die('Nope.');
 }
 
-if (count($argv) < 3) {
-    die("Usage: generate_icons.php <configs folder> <rp folder>\n");
+if (count($argv) < 4) {
+    die("Usage: generate_icons.php <configs folder> <rp folder> <example>\n");
 }
 
 require "spyc.php";
 
 $configRoot = $argv[1];
 $rpFolder = $argv[2];
+$example = $argv[3];
 
 // Prepare path to spell modes
 $spellModelsFolder = "$rpFolder/src/default/assets/magic/models/icons/spells";
+$minecraftModelsFolder = "$rpFolder/src/default/assets/minecraft/models/item";
 
 // Prepare output folder
-$iconsOutputFolder =  "$configRoot/defaults/icons";
+$iconsOutputFolder =  "$configRoot/examples/$example/icons";
 if (!file_exists($iconsOutputFolder)) {
     mkdir($iconsOutputFolder);
 }
@@ -34,20 +36,101 @@ $guiMessages = $guiMessages['gui'];
 $iconMessages = $guiMessages['icons'];
 $spellMessages = $iconMessages['spells'];
 
+// Map all spell models to their texture
+$modelTextures = array();
+$modelFiles = new DirectoryIterator($spellModelsFolder);
+foreach ($modelFiles as $fileInfo) {
+    if ($fileInfo->isDir()) continue;
+    if ($fileInfo->isDot()) continue;
+    $extension = $fileInfo->getExtension();
+    if ($extension != 'json') continue;
+    $fileName = $fileInfo->getFilename();
+    $baseModel = basename($fileName, '.json');
+    $model = json_decode(file_get_contents($spellModelsFolder . '/' . $fileName), true);
+    if (!$model) {
+        echo "! Could not parse $fileName";
+        continue;
+    }
+
+    if (!isset($model['textures']) || !isset($model['textures']['texture'])) {
+        echo "! Model has no textures: $fileName";
+        continue;
+    }
+    $texture = $model['textures']['texture'];
+    $texture = substr($texture, strlen('magic:icons/spells/'));
+    $modelTextures[$baseModel] = $texture;
+}
+
+// Find all RP spell icons in use
+$customItems = array();
+$modelFiles = new DirectoryIterator($minecraftModelsFolder);
+foreach ($modelFiles as $fileInfo) {
+    if ($fileInfo->isDir()) continue;
+    if ($fileInfo->isDot()) continue;
+    $extension = $fileInfo->getExtension();
+    if ($extension != 'json') continue;
+    $fileName = $fileInfo->getFilename();
+    $baseModel = basename($fileName, '.json');
+    $model = json_decode(file_get_contents($minecraftModelsFolder . '/' . $fileName), true);
+    if (!$model) {
+        echo "! Could not parse $fileName";
+        continue;
+    }
+
+    if (!isset($model['overrides'])) {
+        echo "! Model has no overrides: $fileName";
+        continue;
+    }
+    $overrides = $model['overrides'];
+    foreach ($overrides as $override) {
+        if (!isset($override['predicate']) || !isset($override['model'])) continue;
+        $model = $override['model'];
+        if (strpos($model, 'magic:icons/spells/') !== 0) continue;
+        $model = str_replace('magic:icons/spells/', '', $model);
+        $predicate = $override['predicate'];
+        if (!isset($predicate['custom_model_data'])) continue;
+        $customModelData = $predicate['custom_model_data'];
+        $modelKey = $baseModel . '{' . $customModelData . '}';
+        $customItems[$modelKey] = $model;
+    }
+}
+
 // Map all spell icons
-$icons = array();
-$spellIcons = array();
-$unmappedSpellIcons = array();
+$spells = array();
+$spellItems = array();
+
+function my_json_decode($s, $asArray) {
+    $s = str_replace(
+        array('"',  "'"),
+        array('\"', '"'),
+        $s
+    );
+    $s = preg_replace('/(\w+):/i', '"\1":', $s);
+    $s = preg_replace('/:(\w+)/i', ':"\1"', $s);
+
+    return json_decode($s, $asArray);
+}
 
 function convertIcon($icon) {
     $startJson = strpos($icon, '{');
     if (!$startJson) return $icon;
     $jsonPart = substr($icon, $startJson);
-    $json = json_decode($jsonPart, true);
+    $json = my_json_decode($jsonPart, true);
     if (!$json) return $icon;
     if (count($json) != 1 || !isset($json['CustomModelData'])) return $icon;
     $customModelData = $json['CustomModelData'];
-    return substr($icon, 0, $startJson - 1) . "{$customModelData}";
+    return substr($icon, 0, $startJson) . '{' . $customModelData . '}';
+}
+
+function getBaseIcon($icon) {
+    $startJson = strpos($icon, '{');
+    if (!$startJson) return $icon;
+    $jsonPart = substr($icon, $startJson);
+    $json = my_json_decode($jsonPart, true);
+    if (!$json) return $icon;
+    if (!isset($json['CustomModelData'])) return $icon;
+    $customModelData = $json['CustomModelData'];
+    return substr($icon, 0, $startJson) . '{' . $customModelData . '}';
 }
 
 $allConfigFiles = array();
@@ -65,33 +148,22 @@ function gatherFiles($path) {
     }
 }
 
-gatherFiles("$configRoot/examples/survival/spells");
-gatherFiles("$configRoot/examples/engineering/spells");
-gatherFiles("$configRoot/examples/automata/spells");
-gatherFiles("$configRoot/examples/potter/spells");
-gatherFiles("$configRoot/examples/bending/spells");
-gatherFiles("$configRoot/examples/stars/spells");
-gatherFiles("$configRoot/examples/war/spells");
+gatherFiles("$configRoot/examples/$example/spells");
 
 foreach ($allConfigFiles as $filePath) {
     $filePieces = explode('/', $filePath);
     $fileName = end($filePieces);
-    $spellKey = basename($fileName, '.yml');
-    $spellConfig = spyc_load_file($filePath);
-    if (!$spellConfig) {
-        echo "Could not parse $filePath\n";
+    $spellConfigFile = spyc_load_file($filePath);
+    if (!$spellConfigFile) {
+        echo "! Could not parse $filePath\n";
         continue;
     }
-    if (!isset($spellConfig[$spellKey])) {
-        echo "No spell key in $filePath\n";
-        continue;
-    }
-    $spellConfig = $spellConfig[$spellKey];
-    if (!isset($spellConfig['icon'])) {
-        echo "No icon in $filePath\n";
-        continue;
-    }
-    if (isset($spellMessages[$spellKey])) {
+    foreach ($spellConfigFile as $spellKey => $spellConfig) {
+        if (!isset($spellConfig['icon'])) {
+            # echo "No icon in $filePath\n";
+            continue;
+        }
+
         $iconItem = convertIcon($spellConfig['icon']);
         $icon = array('item' => $iconItem);
         if (isset($spellConfig['icon_disabled'])) {
@@ -110,66 +182,63 @@ foreach ($allConfigFiles as $filePath) {
             $icon['url_disabled'] = $spellConfig['icon_disabled_url'];
         }
 
-        echo "Creating icon $spellKey with $iconItem\n";
-        $spellIcons[$iconItem] = $spellKey;
-        $icons[$spellKey] = $icon;
-    } else {
-        echo "Deferring $spellKey\n";
-        $unmappedSpellIcons[$spellKey] = $spellConfig;
+        # echo "Found spell $spellKey with icon $iconItem\n";
+        $spellItems[$iconItem] = $spellKey;
+        $spells[$spellKey] = $icon;
     }
 }
 
-// Check for any uniqueness we need to handle
-$mappedSpellIcons = array();
-foreach ($unmappedSpellIcons as $spellKey => $unmapped) {
-    $iconItem = convertIcon($unmapped['icon']);
-    // Check for models
-    if (!isset($spellIcons[$iconItem]) && file_exists("$spellModelsFolder/$spellKey.json")) {
-        $spellModel = json_decode(file_get_contents("$spellModelsFolder/$spellKey.json"), true);
-        if ($spellModel) {
-            $texture = $spellModel['textures']['texture'];
-            $texture = substr($texture, strlen('magic:icons/spells/'));
-            if (isset($icons[$texture])) {
-                echo "** Mapping $spellKey to $texture from model\n";
-            } else {
-                echo "*** $spellKey has unknown texture in its model: $texture\n";
-            }
-        } else {
-            echo "Couldn't parse model json at $spellModelsFolder/$spellKey.json\n";
-        }
-    }
-    if (isset($spellIcons[$iconItem])) {
-        $mappedIcon = $spellIcons[$iconItem];
-        echo "Mapping $spellKey to icon of $mappedIcon\n";
-        $mappedSpellIcons[$spellKey] = $mappedIcon;
-    } else {
-        echo "* Found unmapped icon: $spellKey as $iconItem\n";
-    }
-}
-
-// Any glyphs we missed (not in a spell by that name)?
+// Check for any unused glyphs
 foreach ($spellMessages as $key => $spellMessage) {
     if (isset($icons[$key])) continue;
 
-    echo "Adding icon for unmapped glyph $key\n";
-    $icons[$key] = array();
+    # echo "! Unused glyph: $key\n";
 }
 
-ksort($icons);
-$iconCount = count($icons);
+// Create one icon for every model used in this configuration
+$usedModels = array();
+foreach ($spells as $spellKey => $spellIcon) {
+    $baseIcon = getBaseIcon($spellIcon['item']);
+    if (!isset($customItems[$baseIcon])) {
+        // echo "! Spell $spellKey uses unknown icon: " . $spellIcon['item'] . " ($baseIcon)\n";
+        continue;
+    }
+
+    $modelKey = $customItems[$baseIcon];
+    $iconKey = isset($spellMessages[$spellKey]) ? $spellKey : $modelKey;
+
+    if (isset($usedModels[$iconKey])) {
+        $existing = $usedModels[$iconKey];
+        if ($iconKey == $spellKey) {
+            $spellIcon = array_merge(array(), $existing, $spellIcon);
+        } else {
+            $spellIcon = array_merge($spellIcon, $existing);
+        }
+        $usedModels[$iconKey] = $spellIcon;
+    } else {
+        $usedModels[$iconKey] = $spellIcon;
+    }
+}
+
+$modelCount = count($usedModels);
 $overwrite = true;
-echo "Writing out $iconCount icons\n";
-foreach ($icons as $key => $icon) {
+echo "Writing out $modelCount icons from RP models used by this example\n";
+foreach ($usedModels as $key => $icon) {
     if ($key === 'default') continue;
     $icon['type'] = 'spell';
     $iconFilename = "$iconsOutputFolder/$key.yml";
     if (!$overwrite && file_exists($iconFilename)) {
-        echo "   Skipping $iconFilename\n";
+        # echo "   Skipping $iconFilename\n";
         continue;
     }
-    $icon['glyph'] = '"' . $spellMessages[$key] . '"';
+    $modelTexture = $modelTextures[$key];
+    if (!isset($spellMessages[$modelTexture])) {
+        echo "! Un-glyphed texture: $modelTexture\n";
+    } else {
+        $icon['glyph'] = '"' . $spellMessages[$modelTexture] . '"';
+    }
     $iconFile = array();
     $iconFile[$key] = $icon;
     file_put_contents($iconFilename, Spyc::YAMLDump($iconFile, false, 0, true));
-    # echo "Wrote to $iconFilename\n";
+    echo "Wrote to $iconFilename\n";
 }
